43642e2c8facbcda295ec9b11763491d7ae3238f
santicomp2014 <santicomp2@gmail.com>
2020-01-16 11:10:02 -0300

    added endpoint/address for tests

--- resolver/resolver_test.go
+++ resolver/resolver_test.go
@@ -5,14 +5,19 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	config "github.com/rnsdomains/rns-go-lib/config"
 )
 
 type ResolveAddressTestCases struct {
+	networkAddress  string
+	resolverAddress string
 	domainToResolve string
 	expectedAddress common.Address
 }
 
 type ResolveContentTestCase struct {
+	networkAddress  string
+	resolverAddress string
 	domainToResolve string
 	expectedHash    common.Hash
 }
@@ -20,13 +25,13 @@ type ResolveContentTestCase struct {
 func TestResolveDomainAddress(t *testing.T) {
 	// if any of these fail, verify their correctness in RNS manager and update if necessary
 	testCases := []ResolveAddressTestCases{
-		{"alecavallero.rsk", common.HexToAddress("0xa78c937844b27bec024f042dcbe5b85d2b7344f6")},
-		{"martin.rsk", common.HexToAddress("0xfb530616391cb526387bad651594bc21a77d3dfe")},
-		{"pedro.rsk", common.HexToAddress("0x0164be16739135950c2fea0e75c98123f7ca06cf")},
-		{"ny.consensus.rsk", common.HexToAddress("0xdbb8fd0a18fd84ba548a7e00e86465fe3de869f8")},
-		{"marcelosdomain.rsk", common.HexToAddress("0xfF33bC3B7324C2A808A9D415935f8D991E6C406c")},
-		{"unregistered.rsk", common.HexToAddress("0x0000000000000000000000000000000000000000")},
-		{"invalidom.ain", common.HexToAddress("0x0000000000000000000000000000000000000000")},
+		{"https://public-node.rsk.co", "0x99a12be4C89CbF6CFD11d1F2c029904a7B644368", "alecavallero.rsk", common.HexToAddress("0xa78c937844b27bec024f042dcbe5b85d2b7344f6")},
+		{"https://public-node.rsk.co", "0x99a12be4C89CbF6CFD11d1F2c029904a7B644368", "martin.rsk", common.HexToAddress("0xfb530616391cb526387bad651594bc21a77d3dfe")},
+		{"https://public-node.rsk.co", "0x99a12be4C89CbF6CFD11d1F2c029904a7B644368", "pedro.rsk", common.HexToAddress("0x0164be16739135950c2fea0e75c98123f7ca06cf")},
+		{"https://public-node.rsk.co", "0x99a12be4C89CbF6CFD11d1F2c029904a7B644368", "ny.consensus.rsk", common.HexToAddress("0xdbb8fd0a18fd84ba548a7e00e86465fe3de869f8")},
+		{"https://public-node.rsk.co", "0x99a12be4C89CbF6CFD11d1F2c029904a7B644368", "marcelosdomain.rsk", common.HexToAddress("0xfF33bC3B7324C2A808A9D415935f8D991E6C406c")},
+		{"https://public-node.rsk.co", "0x99a12be4C89CbF6CFD11d1F2c029904a7B644368", "unregistered.rsk", common.HexToAddress("0x0000000000000000000000000000000000000000")},
+		{"https://public-node.rsk.co", "0x99a12be4C89CbF6CFD11d1F2c029904a7B644368", "invalidom.ain", common.HexToAddress("0x0000000000000000000000000000000000000000")},
 	}
 	resolveDomainAddressTestCases(t, testCases)
 }
@@ -35,6 +40,7 @@ func resolveDomainAddressTestCases(t *testing.T, testCases []ResolveAddressTestC
 	var emptyAddress common.Address
 	for _, testCase := range testCases {
 		t.Run(fmt.Sprint(testCase.domainToResolve), func(t *testing.T) {
+			config.SetConfiguration(testCase.networkAddress, testCase.resolverAddress)
 			resolvedAddress, resolutionError := ResolveDomainAddress(testCase.domainToResolve)
 			if testCase.expectedAddress != resolvedAddress {
 				t.Errorf("Expected address %v and got %v.", testCase.expectedAddress, resolvedAddress)
@@ -54,10 +60,11 @@ func resolveDomainAddressTestCases(t *testing.T, testCases []ResolveAddressTestC
 func TestResolveDomainContent(t *testing.T) {
 	// if any of these fail, verify their correctness in RNS manager and update if necessary
 	testCases := []ResolveContentTestCase{
-		{"vojtech.rsk", common.HexToHash("0x625f47dcda50ad052c620d2f63bd8ffc14f1184833b2f11876e21dc02df393f7")},
-		{"marcelosdomain.rsk", common.HexToHash("0x88ced8ba8e9396672840b47e332b33d6679d9962d80cf340d3cf615db23d4e07")},
-		{"unregistered.rsk", common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000")},
-		{"invalidom.ain", common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000")},
+		{"https://public-node.rsk.co", "0x99a12be4C89CbF6CFD11d1F2c029904a7B644368", "vojtech.rsk", common.HexToHash("0x625f47dcda50ad052c620d2f63bd8ffc14f1184833b2f11876e21dc02df393f7")},
+		{"https://public-node.rsk.co", "0x99a12be4C89CbF6CFD11d1F2c029904a7B644368", "marcelosdomain.rsk", common.HexToHash("0x88ced8ba8e9396672840b47e332b33d6679d9962d80cf340d3cf615db23d4e07")},
+		{"https://public-node.testnet.rsk.co", "0x404308f2a2eec2cdc3cb53d7d295af11c903414e", "santiagosdomain.rsk", common.HexToHash("0x88ced8ba8e9396672840b47e332b33d6679d9962d80cf340d3cf615db23d4e07")},
+		{"https://public-node.rsk.co", "0x99a12be4C89CbF6CFD11d1F2c029904a7B644368", "unregistered.rsk", common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000")},
+		{"https://public-node.rsk.co", "0x99a12be4C89CbF6CFD11d1F2c029904a7B644368", "invalidom.ain", common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000")},
 	}
 	resolveDomainContentTestCases(t, testCases)
 }
@@ -66,6 +73,7 @@ func resolveDomainContentTestCases(t *testing.T, testCases []ResolveContentTestC
 	var emptyContent [32]byte
 	for _, testCase := range testCases {
 		t.Run(fmt.Sprint(testCase.domainToResolve), func(t *testing.T) {
+			config.SetConfiguration(testCase.networkAddress, testCase.resolverAddress)
 			resolvedContent, resolutionError := ResolveDomainContent(testCase.domainToResolve)
 			if testCase.expectedHash != resolvedContent {
 				t.Errorf("Expected hash %v and got %v.", testCase.expectedHash, resolvedContent)
